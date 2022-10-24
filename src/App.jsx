78f31d318c3b3ba4a9576f800db0093de40c98da
damaestro165 <damaestro165@gmail.com>
2022-10-24 12:21:49 +0100

    Added Style To the Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/MainSection";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <Main />
       <Footer />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Header.scss";
+import shareDesktop from "../../assets/shareDesktop.png";
+import ProfileImg from "../../assets/images/profileImage.jpg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="profile-section">
+      <div className="shareIcon">
+        <img src={shareDesktop} alt="shareIcon" />
+      </div>
+      <div>
+        <img src={ProfileImg} alt="Profile_Image" id="profile_img" />
+      </div>
+      <div className="username">
+        <p id="twitter">@yomi_dev</p>
+        <p id="slack">Abayomi</p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
