f71464292b63da7e7eaca081a4b41013048a3313
damaestro165 <damaestro165@gmail.com>
2022-11-04 21:03:16 +0100

    feat: updated the form validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import './ContactForm.scss';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const ContactForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',
+      terms: '',
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required('Please enter your first name'),
+      lastName: Yup.string().required('Please enter your first name'),
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Please enter your email address'),
+      message: Yup.string().required('Please enter a message'),
+      terms: Yup.array().required('Terms of service must be checked'),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      console.log('form submitted');
+      console.log(values);
+      resetForm();
+    },
+  });
+
   return (
     <div className='contact-form'>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div className='text-container'>
           <h2>Contact Me</h2>
           <p>Hi there, contact me to ask me about anything you have in mind.</p>
@@ -12,32 +38,86 @@ const ContactForm = () => {
         <div className='input-container'>
           <div className='name-container'>
             <div className='column'>
-              <label htmlfor='first_name'>First name</label>
-              <input placeholder='Enter your first name ' id='first_name' />
+              <label htmlFor='first_name'>First name</label>
+              <input
+                placeholder='Enter your first name '
+                id='first_name'
+                name='firstName'
+                type='text'
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+                onBlur={formik.handleBlur}
+              />
+              <p className='error'>
+                {formik.touched.firstName && formik.errors.firstName
+                  ? formik.errors.firstName
+                  : ''}
+              </p>
             </div>
             <div className='column'>
-              <label htmlfor='last_name'>Last name</label>
-              <input placeholder='Enter your last name ' id='last_name' />
+              <label htmlFor='last_name'>Last name</label>
+              <input
+                placeholder='Enter your last name '
+                id='last_name'
+                name='lastName'
+                type='text'
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+                onBlur={formik.handleBlur}
+              />
+              <p className='error'>
+                {formik.touched.lastName && formik.errors.lastName
+                  ? formik.errors.lastName
+                  : ''}
+              </p>
             </div>
           </div>
           <div className='email-container column'>
-            <label htmlfor='email'>Email</label>
-            <input placeholder='yourname@email.com' id='email' />
+            <label htmlFor='email'>Email</label>
+            <input
+              placeholder='yourname@email.com'
+              id='email'
+              type='email'
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              onBlur={formik.handleBlur}
+            />
+            <p className='error'>
+              {formik.touched.email && formik.errors.email
+                ? formik.errors.email
+                : ''}
+            </p>
           </div>
           <div className='textarea-container column'>
-            <label htmlfor='message'>Message</label>
+            <label htmlFor='message'>Message</label>
             <textarea
               placeholder="Send me a message and I'll reply you as soon as possible..."
               id='message'
+              onChange={formik.handleChange}
+              value={formik.values.message}
+              onBlur={formik.handleBlur}
             ></textarea>
+            <p className='error'>
+              {formik.touched.message && formik.errors.message
+                ? formik.errors.message
+                : ''}
+            </p>
           </div>
           <div className='checkbox-container'>
-            <input type='checkbox' id='checkbox' />
-            <label htmlFor='checkbox'>
+            <input
+              type='checkbox'
+              id='terms'
+              value='checked'
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            <label htmlFor='terms'>
               You agree to providing your data to name who may contact you.
             </label>
           </div>
-          <button id='btn__submit'>Send message</button>
+          <button id='btn__submit' type='submit' disabled={!formik.isValid}>
+            Send message
+          </button>
         </div>
       </form>
     </div>
